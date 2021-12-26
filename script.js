edaf63f00d77a0cb3704838016e46278493d9203
koppula Narayana Reddy <60657263+ko245@users.noreply.github.com>
2021-12-26 06:20:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function compute()
 {
-    var principal = parseFloat(document.getElementById("principal").value);
-    var rate = parseFloat(document.getElementById("rate").value);
-    var years=parseInt(document.getElementById("years").value);
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years=document.getElementById("years").value;
     var interest= principal*years*rate/100;
-    var year= new Date().getFullyear()+years;
-    document.getElementById("result").innerHTML="Interest: If you deposit <mark>"+principal+", </mark> <br> at an interest rate of <mark>"+rate+"%</mark><br>you will receive an amount of <mark>"+amount+",</mark><br>in the year <mark>"+year+"</mark><br>;
+    var year= new Date().getFullyear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
 }
  function updateRate()
 {
@@ -17,7 +17,7 @@ function validateAmount()
         var principal=document.getElementById("principal").value;
         var biggerThanZero=parseInt(principal)>0;
         if(!biggerThanZero){
-            alert("Enter a positive number"):
+            alert("Enter a positive number");
             document.getElementById("principal").focus();
         }
     }
